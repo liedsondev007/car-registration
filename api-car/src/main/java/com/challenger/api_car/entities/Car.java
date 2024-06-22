71775a996f208513afd355870e8610d93ecbbf3a
Liedson Mendes <mendesliedson978@gmail.com>
2024-06-22 19:33:46 -0300

    create entity

--- api-car/src/main/java/com/challenger/api_car/entities/Car.java
+++ api-car/src/main/java/com/challenger/api_car/entities/Car.java
@@ -1,4 +1,19 @@
 package com.challenger.api_car.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "cars")
+@Data
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chassiId;
+    private String model;
+    private String brand;
+    private String color;
+    private String fabricationYear;
+
 }
