7a1602d6d86c99445aa5342e8dfe4162cd92b36c
Liedson Mendes <mendesliedson978@gmail.com>
2024-06-22 19:39:06 -0300

    create dtos

--- api-car/src/main/java/com/challenger/api_car/dtos/CarRequestDTO.java
+++ api-car/src/main/java/com/challenger/api_car/dtos/CarRequestDTO.java
@@ -1,4 +1,17 @@
 package com.challenger.api_car.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class CarRequestDTO {
+
+    @NotBlank
+    private String model;
+    @NotBlank
+    private String brand;
+    @NotBlank
+    private String color;
+    @NotBlank
+    private String fabricationYear;
 }
--- api-car/src/main/java/com/challenger/api_car/dtos/CarResponseDTO.java
+++ api-car/src/main/java/com/challenger/api_car/dtos/CarResponseDTO.java
@@ -1,4 +1,13 @@
 package com.challenger.api_car.dtos;
 
+import lombok.Data;
+
+@Data
 public class CarResponseDTO {
+
+    private Long chassiId;
+    private String model;
+    private String brand;
+    private String color;
+    private String fabricationYear;
 }
