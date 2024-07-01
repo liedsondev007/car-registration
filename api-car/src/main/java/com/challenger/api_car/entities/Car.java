63e7b6d3b63d60f5944fcaed8b23837d1310ab6b
Liedson Mendes <mendesliedson978@gmail.com>
2024-07-01 15:27:06 -0300

    Update entity

--- api-car/src/main/java/com/challenger/api_car/entities/Car.java
+++ api-car/src/main/java/com/challenger/api_car/entities/Car.java
@@ -10,10 +10,19 @@ public class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chassiId")
     private Long chassiId;
+
+    @Column(name = "model", nullable = false)
     private String model;
+
+    @Column(name = "brand", nullable = false)
     private String brand;
+
+    @Column(name = "color", nullable = false)
     private String color;
+
+    @Column(name = "fabricationYear", nullable = false)
     private String fabricationYear;
 
 }
